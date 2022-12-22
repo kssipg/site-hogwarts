d8c770faf2babba0f146e0034f6ba2e260436a83
kssipg <kutiasmr@gmail.com>
2022-12-22 10:30:37 +0000

    Connection string fix

--- pgConfig.js
+++ pgConfig.js
@@ -3,6 +3,6 @@ import pg from "pg"
 const {Pool} = pg
 
 //Создаём пул соединений с сервером.
-export const pool = new Pool({connectionString: process.env.DB_URL,
+export const pool = new Pool({connectionString: "postgres://hogwarts_lyik_user:WylzxD8hdbDQeT2x80HynSMmom61cm8M@dpg-cei12rla499fabgd5ib0-a.oregon-postgres.render.com/hogwarts_lyik",
 	    ssl: { rejectUnauthorized: false}
 })
