3e16aeada904f1ad1cac2fb6e581d307000adf10
kssipg <kutiasmr@gmail.com>
2022-12-22 10:05:03 +0000

    Fix

--- index.js
+++ index.js
@@ -2,49 +2,23 @@ const express = require('express')
 const {pool} = require('./pgConfig')
 const port = process.env.PORT || 8080
 const app = express()
-var async = require('async');
 
 app.get('/', (req, res) =>
 {
-    const faculty_query = `select * from факультет;`
-    const role_query = `select * from роль;`
-    const student_query = `select фамилия, имя, факультет.название as факультет, роль.название as роль from студент, факультет, роль where студент.факультет_id = факультет.id and студент.роль_id = роль.id;`
-    const problems_query = `select * from задание;`
-    const grades_query = `select студент.фамилия, задание.название as задание, оценка from журнал, студент, задание where журнал.задание_id = задание.id and журнал.студент_id = студент.id;`
+    const sql1 = `select * from задание;`
 
-    var final = {};
-    async.series({
-        faculties: function(cb) {
-            pool.query(faculty_query, function (err, resp){
-                cb(err, resp)
-            })
-        },
-        roles: function(cb){
-            pool.query(role_query, function (err, resp){
-                cb(err, resp)
-            })
-        },
-        students: function(cb){
-            pool.query(student_query, function (err, resp){
-                cb(err, resp)
-            })
-        },
-        problems: function(cb){
-            pool.query(problems_query, function (err, resp){
-                cb(err, resp)
-            })
-        },
-        grades: function(cb){
-            pool.query(grades_query, function (err, resp){
-                cb(err, resp)
-            })
+    pool.query(sql1, (err, resp) =>
+    {
+        if(err)
+            console.log('Error:', err)
+        else
+        {
+            res.render('problemset.ejs', {rows: resp.rows})
         }
-    }, function(err, resp) {
-        if (!err) {
-            res.render('problemset.ejs', {faculties: resp.faculties.rows, roles: resp.roles.rows, students: resp.students.rows, problems: resp.problems.rows, grades: resp.grades.rows})
-            console.log(resp.students.rows[0])
-        }
-    });
+    })
+
 })
 
-app.listen(port, () => console.log('Приложение слушает порт:', port))
+app.listen(port, ()=>
+    console.log('Приложение слушает порт:', port)
+    )
